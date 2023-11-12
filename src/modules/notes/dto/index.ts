8a83cad1ff7c43f13dc15416d42830703885b691
Dragons0458 <juacase5826@gmail.com>
2023-11-11 22:06:16 -0500

    Updated export and added a change to getRepository function to add some types.

--- src/modules/notes/dto/index.ts
+++ src/modules/notes/dto/index.ts
@@ -1,4 +1,5 @@
 export * from './create-note.dto';
 export * from './delete-note-by-id.dto';
 export * from './get-note-by-id.dto';
+export * from './note-id.dto';
 export * from './update-note.dto';
--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,32 +1,37 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+import * as dotenv from 'dotenv';
+import 'reflect-metadata';
+import { DataSource, Repository } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { ObjectLiteral } from 'typeorm/common/ObjectLiteral';
+import { EntityTarget } from 'typeorm/common/EntityTarget';
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    schema: "public",
-    synchronize: false,
-    logging: true,
-    namingStrategy: new SnakeNamingStrategy(),
-    entities: [__dirname + "/../src/entities/*.entity{.ts,.js}"],
-    migrations: [__dirname + "/../src/migrations/*{.ts,.js}"],
-})
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  schema: 'public',
+  synchronize: false,
+  logging: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [__dirname + '/../src/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../src/migrations/*{.ts,.js}'],
+});
 
 export const getDataSource = async () => {
-    if (!AppDataSource.isInitialized) await AppDataSource.initialize()
-    return AppDataSource
-}
+  if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+  return AppDataSource;
+};
 
-export const getRepository = async (entity: any) => {
-    const ds = await getDataSource()
-    return ds.getRepository(entity)
-}
+export const getRepository = async <T extends ObjectLiteral>(
+  entity: EntityTarget<T>,
+): Promise<Repository<T>> => {
+  const ds = await getDataSource();
+  return ds.getRepository(entity);
+};
 
-export default AppDataSource
+export default AppDataSource;
