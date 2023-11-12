acc9b278dd841fbb1c050f61306141b3e32c274e
Dragons0458 <juacase5826@gmail.com>
2023-11-11 23:36:38 -0500

    Added notes service with tests.

---
+++ src/modules/notes/notes.service.spec.ts
@@ -0,0 +1,108 @@
+import { faker } from '@faker-js/faker';
+import { CreateNoteDto, NoteIdDto, UpdateNoteDto } from './dto';
+import { QueryError } from '../../lib/query-error';
+import { getRepository } from '../../orm.config';
+import { NotesService } from './notes.service';
+import { NotesEntity } from '../../entities/notes.entity';
+
+jest.mock('../../orm.config');
+
+const error = new QueryError(404, 'Not found');
+const MockedGetRepository = getRepository as jest.MockedFunction<
+  typeof getRepository
+>;
+
+const mockRepository = {
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  insert: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
+
+MockedGetRepository.mockImplementation(() => mockRepository as never);
+
+describe('NoteService', () => {
+  let noteService: NotesService;
+
+  beforeEach(async () => {
+    noteService = await NotesService.getInstance();
+
+    for (const fn of Object.values(mockRepository)) {
+      fn.mockClear();
+    }
+  });
+
+  it('should return an instance of NotesService and the repository should be initialized', async () => {
+    expect.assertions(2);
+    expect(noteService).toBeInstanceOf(NotesService);
+    expect(noteService['notesRepository']).toBeInstanceOf(Object);
+  });
+
+  it('all method, should return an array', () => {
+    mockRepository.find.mockResolvedValueOnce([]);
+
+    expect(noteService.all()).resolves.toBeInstanceOf(Array);
+  });
+
+  describe('getById method', () => {
+    it('should return a NoteEntity instance', () => {
+      mockRepository.findOneBy.mockResolvedValueOnce(new NotesEntity());
+
+      expect(noteService.getById(faker.string.uuid())).resolves.toBeInstanceOf(
+        NotesEntity,
+      );
+    });
+
+    it('should throw an error with status 404 when the id is not found', () => {
+      mockRepository.findOneBy.mockResolvedValueOnce(null);
+
+      expect(noteService.getById(faker.string.uuid())).rejects.toThrow(error);
+    });
+  });
+
+  it('create method should return a NoteId instance', async () => {
+    expect.assertions(2);
+
+    const noteId = new NoteIdDto();
+    noteId.id = faker.string.uuid();
+    mockRepository.insert.mockResolvedValueOnce({ identifiers: [noteId] });
+
+    const res = await noteService.create({} as CreateNoteDto);
+
+    expect(res).toBeInstanceOf(Object);
+    expect(res.id).toStrictEqual(noteId.id);
+  });
+
+  describe('update method', () => {
+    it("shouldn't return an error when the id is found", () => {
+      mockRepository.update.mockResolvedValueOnce({ affected: 1 });
+
+      expect(
+        noteService.update(faker.number.int(), {} as UpdateNoteDto),
+      ).resolves.not.toThrow();
+    });
+
+    it('should throw an error with status 404 when the id is not found', () => {
+      mockRepository.update.mockResolvedValueOnce({ affected: 0 });
+
+      expect(
+        noteService.update(faker.number.int(), {} as UpdateNoteDto),
+      ).rejects.toThrow(error);
+    });
+  });
+
+  describe('delete method', () => {
+    it("shouldn't return an error when the id is found", () => {
+      mockRepository.delete.mockResolvedValueOnce({ affected: 1 });
+
+      expect(noteService.delete(faker.number.int())).resolves.not.toThrow();
+    });
+
+    it('should throw an error with status 404 when the id is not found', () => {
+      mockRepository.delete.mockResolvedValueOnce({ affected: 0 });
+
+      expect(noteService.delete(faker.number.int())).rejects.toThrow(error);
+    });
+  });
+});
---
+++ src/modules/notes/notes.service.ts
@@ -0,0 +1,93 @@
+import { Repository } from 'typeorm';
+import { NotesEntity } from '../../entities/notes.entity';
+import { QueryError } from '../../lib/query-error';
+import { getRepository } from '../../orm.config';
+import { CreateNoteDto, NoteIdDto, UpdateNoteDto } from './dto';
+
+/**
+ * The notes service class is responsible for handling the business logic of the notes entity.
+ */
+export class NotesService {
+  /**
+   * The notes service instance.
+   * @private
+   */
+  private static notesServiceInstance: NotesService | undefined;
+
+  /**
+   * The notes repository instance.
+   * @private
+   */
+  private notesRepository: Repository<NotesEntity> | undefined;
+
+  private constructor() {}
+
+  static async getInstance(): Promise<NotesService> {
+    if (!this.notesServiceInstance) {
+      this.notesServiceInstance = new NotesService();
+      this.notesServiceInstance.notesRepository =
+        await getRepository(NotesEntity);
+    }
+
+    return this.notesServiceInstance;
+  }
+
+  /**
+   * Returns all notes.
+   * @returns {Promise<NotesEntity[]>} The notes.
+   */
+  async all(): Promise<NotesEntity[]> {
+    return this.notesRepository!.find();
+  }
+
+  /**
+   * Returns a note by id.
+   * @param id {string} The note id.
+   * @returns {Promise<NotesEntity>} The note.
+   */
+  async getById(id: string): Promise<NotesEntity> {
+    const note = await this.notesRepository!.findOneBy({ id });
+
+    if (!note) throw new QueryError(404, 'Not found');
+
+    return note;
+  }
+
+  /**
+   * Creates a new note.
+   * @param createNote {CreateNoteDto} The note data.
+   * @returns {Promise<NoteIdDto>} The note id.
+   */
+  async create(createNote: CreateNoteDto): Promise<NoteIdDto> {
+    const {
+      identifiers: [note],
+    } = await this.notesRepository!.insert(createNote);
+
+    return { id: note?.id };
+  }
+
+  /**
+   * Updates a note.
+   * @param id {number} The note id.
+   * @param updateNote {UpdateNoteDto} The note data.
+   * @throws {QueryError} If the note is not found.
+   * @returns {Promise<void>}
+   */
+  async update(id: number, updateNote: UpdateNoteDto): Promise<void> {
+    const { affected } = await this.notesRepository!.update(id, updateNote);
+
+    if (!affected) throw new QueryError(404, 'Not found');
+  }
+
+  /**
+   * Deletes a note.
+   * @param id {number} The note id.
+   * @throws {QueryError} If the note is not found.
+   * @returns {Promise<void>}
+   */
+  async delete(id: number): Promise<void> {
+    const { affected } = await this.notesRepository!.delete(id);
+
+    if (!affected) throw new QueryError(404, 'Not found');
+  }
+}
