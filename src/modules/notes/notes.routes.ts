12395999a1cae5b184cdb4912d90429aed931e8f
Dragons0458 <juacase5826@gmail.com>
2023-11-12 01:14:35 -0500

    Added routes.

--- src/modules/notes/notes.routes.ts
+++ src/modules/notes/notes.routes.ts
@@ -1,5 +1,57 @@
 import router from '../../lib/router';
+import { validateAndTransform } from '../../lib/input-validator-middleware';
+import { NotesController } from './notes.controller';
+import {
+  CreateNoteDto,
+  DeleteNoteByIdDto,
+  GetNoteByIdDto,
+  UpdateNoteDto,
+} from './dto';
 
-// You can create your routes here
+router.get('/', async (req, res) => {
+  const notesController = await NotesController.getInstance();
+
+  await notesController.all(req, res);
+});
+
+router.get(
+  '/:id',
+  validateAndTransform({ params: GetNoteByIdDto }),
+  async (req, res) => {
+    const notesController = await NotesController.getInstance();
+
+    await notesController.getById(req, res);
+  },
+);
+
+router.post(
+  '/',
+  validateAndTransform({ body: CreateNoteDto }),
+  async (req, res) => {
+    const notesController = await NotesController.getInstance();
+
+    await notesController.create(req, res);
+  },
+);
+
+router.put(
+  '/:id',
+  validateAndTransform({ body: UpdateNoteDto, params: GetNoteByIdDto }),
+  async (req, res) => {
+    const notesController = await NotesController.getInstance();
+
+    await notesController.update(req, res);
+  },
+);
+
+router.delete(
+  '/:id',
+  validateAndTransform({ params: DeleteNoteByIdDto }),
+  async (req, res) => {
+    const notesController = await NotesController.getInstance();
+
+    await notesController.delete(req, res);
+  },
+);
 
 export default router;
