399675a3ad479c6dfe386d3c3d6a7effdc0e8ecb
Dragons0458 <juacase5826@gmail.com>
2023-11-22 14:20:12 -0500

    Added missing code.

--- src/modules/notes/handler.ts
+++ src/modules/notes/handler.ts
@@ -1,7 +1,27 @@
 import serverless from 'serverless-http';
 import app from '../../app';
 import router from './notes.routes';
+import { NextFunction, Request, Response } from 'express';
 
 app.use('/notes', router);
+
+app.use((_, res) => {
+  res.status(404).send();
+});
+
+app.use((err: Error, req: Request, res: Response, __: NextFunction) => {
+  const toReturn: { message: string; error?: string } = {
+    message: 'Internal server error',
+  };
+  const { body, params, query } = req;
+
+  console.error(err, { body, params, query }); // It's important to use a logger system.
+
+  if (process.env.NODE_ENV === 'production') {
+    toReturn.error = err.message;
+  }
+
+  res.status(500).json(toReturn);
+});
 
 export const handler = serverless(app);
